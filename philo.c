d2b8347abf5ed1d2d5ecee9d3bb7dd47b139251a
Daniel Esteban Maida <daniel.maida@wsu.edu>
2017-03-29 23:01:32 -0700

    Fixed some issues

--- philo.c
+++ philo.c
@@ -1,5 +1,6 @@
-// Daniel Maida
-// CS 360
+//Daniel Maida
+//CS 360
+//Dining Philosophers Problem with threads.
 
 #define _POSIX_SOURCE
 #include <math.h>
@@ -11,93 +12,99 @@
 #include <sys/types.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <math.h>
 #include "random_r.h"
 
 #define NUM_PHILO 5
+#define TIME_TO_EAT 100
 
 pthread_mutex_t mutex;
 int chopsticks[NUM_PHILO];
 
-void* EatOrThink(void * threadid) {
-  int tid;
-  tid = (long)threadid;
-  unsigned int state = (unsigned int)tid;
-  int time_eating = 0;
-  int time_thinking = 0;
+void getChopSticks(int threadID, int left, int right){
 
-  while (time_thinking < 15) {
-    int time_to_think = randomGaussian_r(11,7,&state);
-    if (time_to_think < 0 ) {
-      time_to_think = 0;
-    }
-    printf("Philosopher %i thinking for %i seconds (total %i)\n", tid,time_to_think, time_thinking);
-    sleep(time_to_think);
-    time_thinking += time_to_think;
+	printf("Philosopher %i is waiting for chopsticks %i and %i\n", threadID, left, right);
+	while(true){
+		if(pthread_mutex_lock(&mutex) != 0){
+			fprintf(stderr, "Error locking the mutex\n");
+			exit(1);
+		}
+		if (chopsticks[left]==-1 && chopsticks[right]==-1){
+			chopsticks[left] = chopsticks[right] = threadID;
+			if(pthread_mutex_unlock(&mutex) != 0){
+				fprintf(stderr, "Error unlocking mutex after picking up chopsticks\n");
+				exit(1);
+			}
+			return;
+		}
+		if(pthread_mutex_unlock(&mutex) != 0){
+			fprintf(stderr, "Error unlocking mutex after not being able to pick up both chopsticks\n");
+			exit(1);
+		}
+		sleep(1);
+	}
+}
 
-	  int left = tid;
-	  int right = (tid + 1) % NUM_PHILO;
+void* eatOrThink(void * threadid) {
+	int threadID = *((int *)threadid);
+	unsigned int state = (unsigned int)threadID;
+	int time_eating = 0;
+	int time_thinking = 0;
+
+	while (time_eating < TIME_TO_EAT) {
+		int time_to_think = randomGaussian_r(11,7,&state);
+		if (time_to_think < 0 ) {
+			time_to_think = 0;
+		}
+		printf("Philosopher %i thinking for %i seconds (total %i)\n", threadID,time_to_think, time_thinking);
+		sleep(time_to_think);
+		time_thinking += time_to_think;
+
+		int left = threadID;
+		int right = (threadID + 1) % NUM_PHILO;
+
 		int time_to_eat = randomGaussian_r(9,3,&state);
 		if (time_to_eat < 0){
 			time_to_eat = 0;
 		}
 
-		while (1)
-		{
-			pthread_mutex_lock(&mutex );
-			if (chopsticks[left]==1 || chopsticks[right]==1) // if any of the 2 chopsticks is in use
-			{
-				pthread_mutex_unlock(&mutex);
-				sleep(1);
-			}
-			else
-				break;
-		}
+		getChopSticks(threadID, left, right);
 
-		chopsticks[left] = 1;
-		chopsticks[right] = 1;  // Grab the chopsticks
-		pthread_mutex_unlock(&mutex);                            // Release the access to chopsticks
-		printf("Philosopher %i eating for %i seconds (total %i)\n", tid, time_to_eat, time_eating);
+		printf("Philosopher %i eating for %i seconds (total %i)\n", threadID, time_to_eat, time_eating);
 		sleep(time_to_eat);
 		time_eating += time_to_eat;
-		chopsticks[left] = 0;
-		chopsticks[right] = 0;
+		chopsticks[left] = chopsticks[right] = -1;
+		printf("Philosopher %i is putting down %i and %i\n", threadID, left, right);
+	}
+	printf("Philosopher %i is leaving the table (Total Eating: %i) (Total Thinking: %i)\n", threadID, time_eating, time_thinking);
+	free(threadid);
+	return 0;
 
-  }
-
-  printf("Philosopher %i is leaving the table\n", tid);
-  pthread_exit(NULL);
-}
-
-void *PrintHello(void *threadid) {
-  long tid;
-  tid = (long)threadid;
-  printf("Hello World! It's me, thread #%ld!\n", tid);
-  pthread_exit(NULL);
 }
 
 void philo(){
- pthread_t threads[NUM_PHILO];
- int rc;
- long t;
+	pthread_t threads[NUM_PHILO];
 
-	 for (int i = 0; i < NUM_PHILO; i++) {
-	 	chopsticks[i] = 0;
-	 }
+	for (int i = 0; i < NUM_PHILO; i++) {
+		chopsticks[i] = -1;
+	}
 
-	for(t=0;t<NUM_PHILO;t++){
-   printf("In main: creating thread %ld\n", t);
-   rc = pthread_create(&threads[t], NULL, EatOrThink, (void *)t);
-   if (rc != 0){
-     fprintf(stderr, "ERROR; return code from pthread_create() is %d\n",rc);
-     exit(-1);
-     }
-   }
- /* Last thing that main() should do */
- pthread_exit(NULL);
+	for(int i = 0; i < NUM_PHILO; i++){
+		int *pt = malloc(sizeof(*pt));
+		*pt = i;
+		if(pthread_create(&threads[i], NULL, eatOrThink, pt)){
+			fprintf(stderr, "ERROR; return code from pthread_create() \n");
+			exit(1);
+		}
+	}
+	for (int i = 0; i < NUM_PHILO; i++){
+		if(pthread_join(threads[i], NULL) != 0){
+			fprintf(stderr, "Failed to join thread: %i\n", i);
+			exit(1);
+		}
+	}
 }
 
 int main(int argc, char* argv[]){
-    philo();
-    return 0;
+	philo();
+	return 0;
 }
